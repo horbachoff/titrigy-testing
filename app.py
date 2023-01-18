761f5bcc0999b5461f653d9814955a41b1aee37c
horbachoff <mr.horbachov@gmail.com>
2023-01-17 19:44:12 -0500

    HTML template and Image logo Fix

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from views import views
 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello"
+app.register_blueprint(views, url_prefix="/")
 
 if __name__ == "__main__":
     app.run(debug=True)
